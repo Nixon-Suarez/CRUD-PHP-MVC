590c7f1464c7844e5ef604b9cbb5ff171dc697f4
Nixon-Suarez <nysuarez@suplos.com>
2025-06-21 15:41:19 -0500

    PLANTILLA PHP base del SISTEMA

--- index.php
+++ index.php
@@ -1,4 +1,17 @@
 <?php
     require_once "./config/app.php";
     require_once "autoload.php";
+
+    if(isset($_GET['views'])){
+        $url = explode("/",$_GET['views']); # explode -> splits a string usinga a delimiter
+    }else{
+        $url=["login"];
+    }
 ?>
+<!DOCTYPE html>
+<html lang="es">
+    <?php require_once "./app/view/inc/head.php";?>
+<body>
+    <?php require_once "./app/view/inc/script.php";?>
+</body>
+</html>
