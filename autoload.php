9fca6364dd553b3defa9cae9e7c2e267e54a6920
Nixon-Suarez <nysuarez@suplos.com>
2025-06-21 15:26:57 -0500

    creacion funcion AUTOLOAD

--- autoload.php
+++ autoload.php
@@ -1,5 +1,10 @@
 <?php
     spl_autoload_register(function($clase){
         $archivo = __DIR__ ."/".$clase.".php";
-
+        $archivo = str_replace("\\", "/", $archivo); 
+        if(is_file($archivo)){
+            require_once $archivo;
+        } else {
+            throw new Exception("Error al cargar la clase: $clase");
+        }
     }); # spl_autoload_register -> permite cargar automáticamente las clases cuando se instancian, sin necesidad de require_once
