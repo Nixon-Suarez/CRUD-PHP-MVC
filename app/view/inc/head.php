ef79ddd3122c3af6a7afd3f6f53d91d5d4b67254
Nixon-Suarez <nysuarez@suplos.com>
2025-06-21 17:51:09 -0500

    FRAMEWORK BULMA CSS y ajuste de APP_URL

--- app/view/inc/head.php
+++ app/view/inc/head.php
@@ -3,3 +3,5 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo APP_NAME;?></title>
 </head>
+<link rel="stylesheet" href="<?php echo APP_URL;?>app/view/css/estilos.css">
+<link rel="stylesheet" href="<?php echo APP_URL;?>app/view/css/bulma.min.css">
--- config/app.php
+++ config/app.php
@@ -1,5 +1,5 @@
 <?php
-    const APP_URL = 'http://localhost/MVC/';
+    const APP_URL = 'http://localhost/php/index.php/completo/MVC/'; //! cambiar dependiendo de donde se encuentre el proyecto
     const APP_NAME = 'MVC - CRUD Application';
     const APP_SESSION_NAME = 'CRUD';
     date_default_timezone_set("America/Bogota");
