f3cc4681f960f058bfc1adbb19a0d58d8aadfb3d
Nixon-Suarez <nysuarez@suplos.com>
2025-06-22 17:24:43 -0500

    cambie el nombre de la carpeta view a views

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
     require_once "./config/app.php";
-    require_once "./app/view/inc/session_start.php";
+    require_once "./app/views/inc/session_start.php";
     require_once "autoload.php";
 
     if(isset($_GET['views'])){
@@ -12,9 +12,21 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
-    <?php require_once "./app/view/inc/head.php";?>
+    <?php require_once "./app/views/inc/head.php";?>
 </head>
 <body>
-    <?php require_once "./app/view/inc/script.php";?>
+    <?php
+        use app\controllers\viewsController;
+
+        $viewsController= new viewsController();
+        $vista=$viewsController->obtenerVistasControlador($url[0]);
+        if($vista=="login" || $vista=="404"){
+            require_once "./app/views/content/".$vista."_view.php";
+        }else{
+            require_once $vista;
+        }
+        
+        require_once "./app/view/inc/script.php";
+    ?>
 </body>
 </html>
