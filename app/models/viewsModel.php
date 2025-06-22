7f5fd7d49bd8cccee7b3b0f5b9e7f85e82264a55
Nixon-Suarez <nysuarez@suplos.com>
2025-06-22 13:49:41 -0500

    Se CREO un CONTROLADOR para las VISTAS

---
+++ app/models/viewsModel.php
@@ -0,0 +1,22 @@
+<?php
+    namespace app\models;
+    class viewsModel{
+        protected function obtenerVistasModelo($vistas){
+            $listaBlanca = [
+                "dashboard",
+            ];
+
+            if(in_array($vistas, $listaBlanca)) {
+                if (is_file("./app/view/content/{$vistas}_view.php")) {
+                    $contenido = "./app/view/content/{$vistas}_view.php";
+                }else {
+                    $contenido = "404";
+                }
+            } elseif ($vistas == "login" || $vistas == "index") {
+                $contenido = "login";
+            } else{
+                $contenido = "404";
+            }
+            return $contenido;
+        }
+    }
