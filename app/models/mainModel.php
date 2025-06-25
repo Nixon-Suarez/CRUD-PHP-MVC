64bd582c70d09c72bc58a1074663de58c15462eb
Nixon-Suarez <nysuarez@suplos.com>
2025-06-24 19:23:18 -0500

    creacion de l modelo principal y conexion a la BD

---
+++ app/models/mainModel.php
@@ -0,0 +1,19 @@
+<?php
+    namespace app\models;
+    use \PDO;
+    if(file_exists(__DIR__."/../../config/server.php")){
+        require_once __DIR__."/../../config/server.php";
+    }
+    class mainModel{
+        private $server = DB_SERVER;
+        private $db = DB_NAME;
+        private $user = DB_USER;
+        private $pass = DB_PASS;
+
+        // conexion BD
+        protected function conexion(){
+            $connect = new PDO("mysql:host=".$this->server.";dbname=".$this->db, $this->user, $this->pass);
+            $connect->exec("SET CHARACTER SET utf8");# define que la codificacion de caracteres sea utf8
+            return $connect;
+        }
+    }
