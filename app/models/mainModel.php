5efe9377a1f94730e4e5af53f7f5046965b5ad72
Nixon-Suarez <nysuarez@suplos.com>
2025-06-25 19:35:18 -0500

    MODELO para BOTONES de PAGINACION

--- app/models/mainModel.php
+++ app/models/mainModel.php
@@ -127,4 +127,46 @@
             $sql->execute();
             return $sql; 
         }
+
+        public function paginadorTablas($pagina, $total_paginas, $url, $botones){
+            $tabla = '<nav class="pagination is-centered is-rounded" role="navigation" aria-label="pagination">';
+
+            if($pagina <= 1){ # si la pagina es menor o igual a 1 se desactiva el boton de anterior
+                $tabla .= '
+                <a class="pagination-previous is-disabled" disabled> Anterior</a>
+                <ul class="pagination-list">
+                ';   
+            }else{ # si la pagina es mayor a 1 se activa el boton de anterior
+                $tabla .= '
+                <a class="pagination-previous" href="'.$url.($pagina-1).'/">Anterior</a>
+                <ul class="pagination-list">
+                    <li><a class="pagination-link" href="'.$url.'.1/">1</a></li> <!-- se crea un boton con el numero 1 y se le asigna la url que lleva a la pagina uno -->
+                    <li><span class="pagination-elliosis">&hellip;</span></li> <!-- son los ... -->
+                '; 
+            }
+
+            $ContadorI = 0;
+            for($i = $pagina; $i <= $total_paginas && $ContadorI < $botones; $i++) { # se inicia un ciclo for que va desde la pagina actual hasta la cantidad de paginas y se limita a la cantidad de botones
+                $tabla .= '<li><a class="pagination-link" href="'.$url.$i.'/">'.$i.'</a></li>'; # se crea un boton con el numero de la pagina y se le asigna la url correspondiente
+                $ContadorI++;
+            }
+            
+            if($pagina == $total_paginas){ # si la pagina es igual a la cantidad de paginas se desactiva el boton de siguiente
+                $tabla .= '
+                </ul>
+                <a class="pagination-next" is-disabled disabled>Siguiente</a>
+                ';   
+            }else{ # si la pagina es menor a la cantidad de paginas se activa el boton de siguiente
+                $tabla .= '
+                    <li><span class="pagination-elliosis">&hellip;</span></li>
+                    <li><a class="pagination-link" href="'.$url.$total_paginas.'/">'
+                    .$total_paginas.'</a></li> <!-- se crea un boton con el numero maximo de paginas y se le asigna la url que lleva a la pagina final -->
+                </ul>
+                <a class="pagination-next" href="'.$url.($pagina+1).'/">Siguiente</a>
+                '; 
+            }
+
+            $tabla .= '</nav>';
+            return $tabla;
+        }
     }
